0f6cd713a8bf277dbb550126fd1c46349aac8242
Dali Yang <cnyangdali@outlook.com>
2021-10-13 10:22:02 +0800

    Correct shell file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo cp -r include/. /usr
-sudo cp -r lib/. /usr
-echo("Environment has been set up.")
+sudo cp -r include/. /usr/include
+sudo cp -r lib/. /usr/include
+echo "Environment has been set up."
