a161552e1ed9041d8a3f245279ba243aeeaea449
Dali Yang <cnyangdali@outlook.com>
2021-10-13 11:16:53 +0800

    test

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+sudo apt-get update
+sudo apt-get install  libmodbus-dev
+sudo apt-get install  wiringpi
+
 MYDIR=`dirname $0`
 sudo cp -r ${MYDIR}/include/. /usr/include
 sudo cp -r ${MYDIR}/lib/. /usr/lib
+
 echo "Environment has been set up from ${MYDIR}."
