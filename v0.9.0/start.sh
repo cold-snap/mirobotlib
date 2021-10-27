44ca970d2289ca3a17e7fdb6ef0c90f43bb0a8ec
Dali Yang <cnyangdali@outlook.com>
2021-10-27 13:39:54 +0800

    Update 61499

--- v0.9.0/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd FBSRT/
-chmod 777 PLCSRT.exe
-chmod +777 "/dev/ttyS0"
-chmod +777 "/dev/ttyUSB0"
-chmod +777 "/dev/gpiomem"
-./PLCSRT.exe 8083 8081 8080 8082 127.0.0.1 &
--- v0.9.0/start_clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /home/FBSRT/
-chmod 777 PLCSRT.exe
-./PLCSRT.exe 8083 8081 8080 8082 127.0.0.1 -noautostart -cleandb -cleanlog &
